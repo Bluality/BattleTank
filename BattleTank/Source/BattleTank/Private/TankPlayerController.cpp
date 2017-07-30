dc6ef081d6957e04aaede76ab1c9f3da5ee622bc
LORIW10\Lori <jb10@hotmail.it>
2017-07-30 03:07:52 +0200

    Barrel pitch

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
-#include "Engine/World.h"
+#include "Public/Engine.h"
+#include "Tank.h"
 
 void ATankPlayerController::BeginPlay()
 {
