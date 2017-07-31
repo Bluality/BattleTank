4c0cd5d31d5eb286c14a0c6816a66a26982322b0
LORIW10\Lori <jb10@hotmail.it>
2017-07-31 20:21:31 +0200

    Auto avanti/indietro

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,10 +2,23 @@
 
 #include "TankMovementComponent.h"
 #include "Engine/World.h"
+#include "TankTrack.h"
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 
 	UE_LOG(LogTemp, Warning, TEXT("%f: Intend move forward throw: %f"), GetWorld()->GetTimeSeconds(), Throw);
+	
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 
+}
+
+void UTankMovementComponent::Initialise(UTankTrack * LeftTrackToSet, UTankTrack * RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) return;
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+
+	// TODO prevent double speed due to double input
 }
