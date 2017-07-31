7aa9000d89997b491326ca1451530a3217f1c736
LORIW10\Lori <jb10@hotmail.it>
2017-07-31 19:07:44 +0200

    Aggiunto il gas

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -9,5 +9,9 @@ void UTankTrack::SetThrottle(float Throttle)
 	UE_LOG(LogTemp, Warning, TEXT("%f: %s throttle is %f"), GetWorld()->GetTimeSeconds(), *GetName(), Throttle);
 
 	// TODO clamp throttle to -1/+1
+	auto ForceApllied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApllied, ForceLocation);
 }
 
