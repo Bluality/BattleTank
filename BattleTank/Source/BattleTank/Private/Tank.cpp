7453d80f1dac70f0fa7543fa84eff8a2b11ab5da
LORIW10\Lori <jb10@hotmail.it>
2017-07-31 16:14:46 +0200

    I proiettili volano!

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -49,14 +49,14 @@ void ATank::SetTurretReference(UTankTurret * TurretToSet)
 
 void ATank::Fire()
 {
-	UE_LOG(LogTemp, Warning, TEXT("%f: Fire!"), GetWorld()->GetTimeSeconds());
-
 	if (!Barrel) return;
 
 	// Spawn a projectile at the socket on the barrel
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 			ProjectileBlueprint,
 			Barrel->GetSocketLocation(FName("Projectile")),
 			Barrel->GetSocketRotation(FName("Projectile"))
 		);
+
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
