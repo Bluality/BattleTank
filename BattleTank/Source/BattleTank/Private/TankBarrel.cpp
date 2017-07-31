bbc24688fd7e03a1b72cba9b7ad43d9869fde261
LORIW10\Lori <jb10@hotmail.it>
2017-07-31 17:57:31 +0200

    Bug fix e pulsante quit

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -2,13 +2,14 @@
 
 #include "TankBarrel.h"
 #include "BattleTank.h"
+#include "Engine/World.h"
 
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// Move barrel the right amount this frame
 	// Given a max elevation speed, and the frame time
-	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1.0f, 1.0f);
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
 	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
 	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
 	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
